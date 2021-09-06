e73abe9dec4a20456dd775d5f1af289924f997cf
f-annassiri <f.annassiri@gmail.com>
2021-09-06 02:02:13 +0100

    first commit

---
+++ hospital_Management/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cancer Hospital Management',
+    'version': '1.0',
+    'summary': ' Cancer Hospital Management Software',
+    'sequence': -100,
+    'description': """ Cancer Hospital Management Software""",
+    'category': 'Productivity',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/patient.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
